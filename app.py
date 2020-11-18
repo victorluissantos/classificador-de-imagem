5696920ee6807590cb42c1e0cac7f7d472e78abf
Victor L. Santos <victorluissantos@live.com>
2020-11-18 10:06:12 -0300

    firt commit with app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+try:
+    import tensorflow as tf
+except:
+    os.system('pip install tensorflow')
+
+try:
+	from tensorflow import keras
+except:
+    os.system('pip install keras')
+
+try:
+    import numpy as np
+except:
+    os.system('pip install numpy')
+
+try:
+    from matplotlib import pyplot as plt
+except:
+    os.system('pip install six')
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['t-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+print(len(train_labels))
+print(train_labels)
+print(test_images.shape)
+print(len(test_labels))
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+	plt.subplot(5,5,i+1)
+	plt.xticks([])
+	plt.yticks([])
+	plt.grid(False)
+	plt.imshow(train_images[i], cmap=plt.cm.binary)
+	plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+model = keras.Sequential([
+		keras.layers.Flatten(input_shape=(28, 28)),
+		keras.layers.Dense(128, activation='relu'),
+		keras.layers.Dense(10, activation='softmax')
+	])
+
+model.compile(optimizer='adam',
+				loss='sparse_categorical_crossentropy',
+				metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+print('\nTest accuracy:',test_acc)
+
+predictions = model.predict(test_images)
+
+print(predictions[0])
